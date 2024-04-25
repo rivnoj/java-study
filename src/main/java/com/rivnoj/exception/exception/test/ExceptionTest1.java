9a93d6df116a8091a1f2cbdd6dd9c63fb1130f68
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-04-25 16:38:38 -0300

    Aula 97 - Exceções pt 03 - Exception

---
+++ src/main/java/com/rivnoj/exception/exception/test/ExceptionTest1.java
@@ -0,0 +1,21 @@
+package com.rivnoj.exception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest1 {
+  public static void main(String[] args) {
+    criarNovoArquivo();
+  }
+
+  private static void criarNovoArquivo() {
+    File file = new File("arquivo/test.txt");
+    
+    try {
+      boolean isCreated = file.createNewFile();
+      System.out.println("Arquivo criado " + isCreated);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
