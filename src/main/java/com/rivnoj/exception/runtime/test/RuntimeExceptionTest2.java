b54aafa23a818ca3e59166c9dd63b1a235507d56
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-04-26 08:37:03 -0300

    Aula 98 - Exceções pt 04 - Lançando exceção unchecked

---
+++ src/main/java/com/rivnoj/exception/runtime/test/RuntimeExceptionTest2.java
@@ -0,0 +1,25 @@
+package com.rivnoj.exception.runtime.test;
+
+public class RuntimeExceptionTest2 {
+  public static void main(String[] args) {
+    try {
+      divisao(1, 0);
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * @param a
+   * @param b não pode ser zero
+   * @return a/b
+   * @throws IllegalArgumentException
+   */
+  private static int divisao(int a, int b) {
+    if (b == 0) {
+      throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+    }
+    
+    return a/b;
+  }
+}
