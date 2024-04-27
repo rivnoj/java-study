6b81fa16d40d9e099984c3807ddd989b24e61440
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-04-27 10:45:09 -0300

    Aula 99 - Exceções pt 05 - Lançando exceção checked

---
+++ src/main/java/com/rivnoj/exception/exception/test/ExceptionTest2.java
@@ -0,0 +1,22 @@
+package com.rivnoj.exception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest2 {
+  public static void main(String[] args) throws IOException {
+    criarNovoArquivo();
+  }
+
+  public static void criarNovoArquivo() throws IOException {
+    File file = new File("arquivo/test.txt");
+    
+    try {
+      boolean isCreated = file.createNewFile();
+      System.out.println("Arquivo criado " + isCreated);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
+    }
+  }
+}
