73fedd1ece1c256c9f473b6f46de82a754351c5e
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-04-29 09:45:20 -0300

    Aula 100 - Exceções pt 06 - Bloco Finally

---
+++ src/main/java/com/rivnoj/exception/runtime/test/RuntimeExceptionTest3.java
@@ -0,0 +1,31 @@
+package com.rivnoj.exception.runtime.test;
+
+public class RuntimeExceptionTest3 {
+  public static void main(String[] args) {
+    System.out.println(abreConexao());
+    abreConexao2();
+  }
+
+  private static String abreConexao() {
+    try {
+      System.out.println("Abrindo arquivo");
+      System.out.println("Escrevendo dados no arquivo");
+      return "Conexão aberta";
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      System.out.println("Liberando recursos do SO");
+    }
+
+    return null;
+  }
+
+  private static void abreConexao2() {
+    try {
+      System.out.println("Abrindo arquivo");
+      System.out.println("Escrevendo dados no arquivo");
+    } finally {
+      System.out.println("Liberando recursos do SO");
+    }
+  }
+}
