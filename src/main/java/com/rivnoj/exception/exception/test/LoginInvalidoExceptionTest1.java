031a62acfef041ef8fad0e967b934277b5d349d1
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-05-01 10:44:22 -0300

    Aula 104 - Exceções pt 10 - Exceção customizada

---
+++ src/main/java/com/rivnoj/exception/exception/test/LoginInvalidoExceptionTest1.java
@@ -0,0 +1,32 @@
+package com.rivnoj.exception.exception.test;
+
+import java.util.Scanner;
+
+import com.rivnoj.exception.exception.dominio.LoginInvalidoException;
+
+public class LoginInvalidoExceptionTest1 {
+  public static void main(String[] args) {
+    try {
+      logar();
+    } catch (LoginInvalidoException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void logar() throws LoginInvalidoException {
+    try (Scanner scanner = new Scanner(System.in)) {
+      String usernameDB = "username";
+      String passwordDB = "pass";
+      System.out.print("Usuário: ");
+      String usernameTyped = scanner.nextLine();
+      System.out.print("Senha: ");
+      String passwordTyped = scanner.nextLine();
+
+      if (!usernameDB.equals(usernameTyped) || !passwordDB.equals(passwordTyped)) {
+        throw new LoginInvalidoException("Usuário ou senha inválidos");
+      }
+
+      System.out.println("Usuário logado com sucesso");
+    }
+  }
+}
