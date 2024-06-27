57f888f8bad95375b95f47a8a01ea288b3c0e650
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-06-27 14:53:57 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/main/java/com/rivnoj/javacore/wrapper/test/WrapperTest1.java
+++ src/main/java/com/rivnoj/javacore/wrapper/test/WrapperTest1.java
@@ -11,6 +11,16 @@ public class WrapperTest1 {
     char charP = 'W';
     boolean booleanP = false;
 
+    System.out.println(byteP);
+    System.out.println(shortP);
+    System.out.println(intP);
+    System.out.println(longP);
+    System.out.println(floatP);
+    System.out.println(doubleP);
+    System.out.println(charP);
+    System.out.println(booleanP);
+
+    //autoboxing
     Byte byteW = 1;
     Short shortW = 1;
     Integer intW = 1;
@@ -19,5 +29,31 @@ public class WrapperTest1 {
     Double doubleW = 10D;
     Character charW = 'W';
     Boolean booleanW = false;
+
+    System.out.println(byteW);
+    System.out.println(shortW);
+    System.out.println(intW);
+    System.out.println(longW);
+    System.out.println(floatW);
+    System.out.println(doubleW);
+    System.out.println(charW);
+    System.out.println(booleanW);
+    
+    //unboxing
+    int i = intW;
+    Integer intW2 = Integer.parseInt("1");
+    boolean verdadeiro = Boolean.parseBoolean("True");
+    
+    System.out.println(i);
+    System.out.println(intW2);
+    System.out.println(verdadeiro);
+
+    System.out.println(Character.isDigit('A'));
+    System.out.println(Character.isDigit('9'));
+    System.out.println(Character.isLetterOrDigit('!'));
+    System.out.println(Character.isUpperCase('A'));
+    System.out.println(Character.isLowerCase('a'));
+    System.out.println(Character.toUpperCase('a'));
+    System.out.println(Character.toLowerCase('A'));
   }
 }
