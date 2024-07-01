790fd82afe961caabe0cc5bdbb03065d0acc75eb
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-01 17:19:36 -0300

    109 - Classes Utilitárias - Strings pt 02

---
+++ src/main/java/com/rivnoj/javacore/string/test/StringTest2.java
@@ -0,0 +1,16 @@
+package com.rivnoj.javacore.string.test;
+
+public class StringTest2 {
+  public static void main(String[] args) {
+    String nome = "  Luffy   ";
+    String numeros = "012345";
+    System.out.println(nome.charAt(0));
+    System.out.println(nome.length());
+    System.out.println(nome.replace('f', 'l'));
+    System.out.println(nome.toLowerCase());
+    System.out.println(nome.toUpperCase());
+    System.out.println(numeros.length());
+    System.out.println(numeros.substring(3, numeros.length()));
+    System.out.println(nome.trim());
+  }
+}
